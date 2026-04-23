c6a7e04df9dcf81f57e335dc664afd179c08f42b
desiro <desiro@users.noreply.github.com>
2026-04-23 23:09:36 +0200

    Update structuresSHAPE.sh

--- structuresSHAPE.sh
+++ structuresSHAPE.sh
@@ -51,7 +51,7 @@ function getStructuresSHAPE() {
     local -n one=$1
     for i in "${!one[@]}"
     do
-        conda run -n strctures_SHAPE python $getStSHP \
+        conda run -n structures_SHAPE python $getStSHP \
             --prefix $resGetStSHP/${one[i]} --overwrite --pickleData --viennaRNAtemperature 32.0 \
             --structureEnergy -6.0 --interactionDistance 200 \
             --genomeFasta $data/${one[i]}.fa --dataSHAPE $data/${one[i]}
@@ -63,8 +63,8 @@ function compareStructuresSHAPE() {
     local -n two=$2
     for i in "${!one[@]}"
     do
-        conda run -n strctures_SHAPE python $cmpStSHP \
-            --prefix $resCmpStSHP/${one[i]}_${two[i]} --overwrite --pickleData --viennaRNAtemperature 32.0 \
+        conda run -n structures_SHAPE python $cmpStSHP \
+            --prefix $resCmpStSHP/${one[i]}_${two[i]} --overwrite --pickleData \
             --genomeFasta1 $data/${one[i]}.fa --dataSHAPE1 $data/${one[i]} --structureFile1 $resGetStSHP/${one[i]}/${one[i]}_subsets.pcl \
             --genomeFasta2 $data/${two[i]}.fa --dataSHAPE2 $data/${two[i]} --structureFile2 $resGetStSHP/${two[i]}/${two[i]}_subsets.pcl
     done
